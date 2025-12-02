3e7136b07f9d60cbea27f66ff50187be9dada73f
AuroraLopez <alopfue679@g.educaand.es>
2025-12-02 13:13:45 +0100

    Enum tipo y jugador

--- src/Clases/Jugador.java
+++ src/Clases/Jugador.java
@@ -1,5 +1,25 @@
 package Clases;
 
 public class Jugador {
-    
+    private String nombre;
+    private int dinero = 100;
+    private Tipo tipo;
+
+    public Jugador(String nombre, Tipo tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombre='" + nombre + '\'' +
+                ", dinero=" + dinero +
+                ", tipo=" + tipo +
+                '}';
+    }
 }
