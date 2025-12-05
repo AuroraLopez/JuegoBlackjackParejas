8591675462a61c1acd358d15a5228aa2d630eee2
AuroraLopez <alopfue679@g.educaand.es>
2025-12-05 14:10:45 +0100

    Terminación y comentario de la grana mayoria de clases

--- src/Clases/Jugador.java
+++ src/Clases/Jugador.java
@@ -1,45 +1,52 @@
 package Clases;
 
+
 public class Jugador {
+
     private String nombre;
     private int dinero = 100;
     private Tipo tipo;
+    private Mano mano = new Mano();
 
-    public Jugador (){
-
-    }
-
+    // Constructor para jugador
     public Jugador(String nombre, Tipo tipo) {
         this.nombre = nombre;
         this.tipo = tipo;
     }
 
-    public void setTipo(Tipo tipo) {
-        this.tipo = tipo;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    // Metodos getter para metodos ajenos
+    public Mano getMano() {
+        return mano;
     }
 
     public int getDinero() {
         return dinero;
     }
 
+        
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    // Función para cuando ganas la apuesta
     public void ganar(int cantidad) {
         dinero += cantidad;
     }
 
+    // Función para cuandon pierdes la apuesta
     public void perder(int cantidad) {
         dinero -= cantidad;
     }
 
+    // Para que se quite todo de la mano
+    public void resetearMano() {
+        mano.limpiar();
+    }
+
+    // Imprimimos jugador
     @Override
     public String toString() {
-        return "Jugador{" +
-                "nombre='" + nombre + '\'' +
-                ", dinero=" + dinero +
-                ", tipo=" + tipo +
-                '}';
+        return nombre + " - Fondos: " + dinero + "€ - Mano: " + mano;
     }
 }
